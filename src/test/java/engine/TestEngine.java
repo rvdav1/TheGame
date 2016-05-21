8595aa8a8246caa43b1ab438ed98b53bcaf6cab5
rvdav1 <rvdav1@freemail.hu>
2016-05-21 04:50:51 +0200

    update test

--- src/test/java/engine/TestEngine.java
+++ src/test/java/engine/TestEngine.java
@@ -10,161 +10,158 @@ import org.junit.Test;
 import pieces.*;
 
 public class TestEngine {
-	Board board = new Board();
-	Game game = new Game();
-	List<String> firstTeam = Arrays.asList("Cavalry","Guard","Infantry","Lancers","Militia","Peasant");
-	List<String> secondTeam = Arrays.asList("Paladin","Raiders","Spearman","Swordsmen","Militia","Peasant");
 
 	@Test
-	public void testZone(){
-		GameZone zone = new GameZone(4,5,true);
-		GameZone zone2 = new GameZone(0,0);
-		zone2.setX(1);
-		zone2.setY(1);
+	public void gameZoneTest(){
+		GameZone zone1 = new GameZone(1,1,true);
+		GameZone zone2 = new GameZone(1,1);
 		
+		assertEquals(1,zone1.getX());
+		zone1.setX(2);
+		assertEquals(2,zone1.getX());
+		assertEquals(1,zone1.getY());
+		zone1.setY(2);
+		assertEquals(2,zone1.getX());
+		assertEquals(true,zone1.isAvailable());
+		zone1.setAvailable(false);
+		assertEquals(false,zone1.isAvailable());
+		zone1.setOwnedBy(true);
+		assertEquals(true,zone1.isOwnedBy());
+		zone1.setOwnedBy(false);
+		assertEquals(false,zone1.isOwnedBy());
+
 		assertEquals(1,zone2.getX());
+		zone2.setX(2);
+		assertEquals(2,zone2.getX());
 		assertEquals(1,zone2.getY());
-		assertEquals(4, zone.getX());
-		assertEquals(5, zone.getY());
-		assertEquals(true, zone.isAvailable());
+		zone2.setY(2);
+		assertEquals(2,zone2.getX());
+		zone2.setAvailable(true);
+		assertEquals(true,zone2.isAvailable());
+		zone2.setAvailable(false);
+		assertEquals(false,zone2.isAvailable());
+		zone2.setOwnedBy(true);
+		assertEquals(true,zone2.isOwnedBy());
+		zone2.setOwnedBy(false);
+		assertEquals(false,zone2.isOwnedBy());
 	}
 	
 	@Test
-	public void testBoard(){
-		assertEquals(10,board.getBoardSize(),0);
-		assertEquals(board.getZones()[4][4],board.getZone(4, 4));
-		board.setBoardSize(10);
-		board.setZones(board.getZones());
-		assertEquals(10,board.getBoardSize(),0);
-		assertEquals(board.getZones()[4][4],board.getZone(4, 4));
-	}
-	
-	@Test
-	public void testUnit(){
-		GameZone zone1 = board.getZone(4, 4);
-		GameZone zone2 = board.getZone(5, 5);
-
-		Unit unit = new Unit(true,zone1);
+	public void boardTest(){
+		Board board = new Board();
 		
-		assertEquals(zone1,unit.getUnitZone());
+		assertEquals(new Board().getZones()[0][0].getX(),board.getZones()[0][0].getX());
+		board.setZones(new Board().getZones());
+		assertEquals(new Board().getZones()[0][0].getX(),board.getZones()[0][0].getX());
+		assertEquals(10,board.getBoardSize());
+		board.setBoardSize(5);
+		assertEquals(5,board.getBoardSize());
+		assertEquals(new Board().getZones()[0][0].getX(),board.getZone(0, 0).getX());
+	}
+	
+	@Test
+	public void unitTest(){
+		Unit unit = new Unit(true,new GameZone(1,1));
+		
+		assertEquals(new GameZone(1,1).getX(),unit.getUnitZone().getX());
+		unit.setUnitZone(new GameZone(2,1));
+		assertEquals(new GameZone(2,1).getX(),unit.getUnitZone().getX());
 		assertEquals(true,unit.isTeam());
-		unit.moveUnit(zone2);
-		assertEquals(zone2,unit.getUnitZone());
 		unit.setTeam(false);
 		assertEquals(false,unit.isTeam());
-		unit.moveUnit(zone1);
-		assertEquals(zone1,unit.getUnitZone());
+		unit.moveUnit(new GameZone(6,1));
+		assertEquals(new GameZone(2,1).getX(),unit.getUnitZone().getX());
 	}
-	
+
 	@Test
-	public void testCombatUnit(){
-		CombatUnit unit1 = new Cavalry(true,board.getZone(0, 0));
-		CombatUnit unit2 = new Guard(false,board.getZone(0, 1));
-		
+	public void combatUnitTest(){
+		CombatUnit unit1 = new Cavalry(true,new GameZone(1,1));
+		CombatUnit unit2 = new Guard(true,new GameZone(1,2));
+		CombatUnit unit3 = new Infantry(true,new GameZone(1,3));
+		CombatUnit unit4 = new Lancers(true,new GameZone(1,4));
+		CombatUnit unit5 = new Militia(true,new GameZone(1,5));
+		CombatUnit unit6 = new Paladin(true,new GameZone(1,6));
+		CombatUnit unit7 = new Peasant(true,new GameZone(1,7));
+		CombatUnit unit8 = new Raiders(true,new GameZone(1,8));
+		CombatUnit unit9 = new Spearman(true,new GameZone(1,9));
+		CombatUnit unit10 = new Swordsmen(true,new GameZone(1,0));
+
 		assertEquals(100,unit1.getHealth());
-		assertEquals(true,unit2.isAlive());
-		unit1.attackTarget(unit2);
-		assertEquals(false,unit2.isAlive());
-		assertEquals(5,unit1.getAtk());
-		assertEquals(1,unit1.getDef());
-		assertEquals(3,unit1.getMoral());
-		assertEquals(14,unit1.getActualAtk());
-		assertEquals(7,unit1.getActualDef());
-		assertEquals("Cavalry",unit1.getUnitName());
+		unit1.setHealth(88);
+		assertEquals(88,unit1.getHealth());
+		assertEquals(1,unit2.getAtk());
+		unit2.setAtk(2);
+		assertEquals(2,unit2.getAtk());
+		assertEquals(3,unit3.getDef());
+		unit3.setDef(4);
+		assertEquals(4,unit3.getDef());
+		assertEquals(4,unit4.getMoral());
+		unit4.setMoral(5);
+		assertEquals(5,unit4.getMoral());
+		assertEquals(true,unit5.isAlive());
+		unit5.setAlive(false);
+		assertEquals(false,unit5.isAlive());
+		assertEquals("Paladin",unit6.getUnitName());
+		unit6.setUnitName("Bobby");
+		assertEquals("Bobby",unit6.getUnitName());
+		assertEquals(10,unit7.getActualAtk());
+		assertEquals(5,unit8.getActualDef());
+		unit9.attackTarget(unit10);
+		assertEquals(true,unit9.isAlive());
 	}
 	
 	@Test
-	public void testGame(){
-		game.initGame(firstTeam, secondTeam);
+	public void gameTest(){
+		Game game = new Game();
+		List<String> units1 = Arrays.asList("Peasant","Cavalry","Guard","Infantry","Lancers","Militia");
+		List<String> units2 = Arrays.asList("Paladin","Raiders","Spearman","Swordsmen","Lancers","Militia");
+		
 		assertEquals(true,game.isFirstPlayer());
-		assertEquals(6,game.getFirstAlive());
-		Board board2 = game.getBoard();
-		
-		game.getFirstTeam()[5].setAlive(false);
-		assertEquals(5,game.getFirstAlive());
-		assertEquals(6,game.getSecondAlive());
-		
-		game.getSecondTeam()[5].setAlive(false);
-		assertEquals(5,game.getSecondAlive());
-		
-		game.firstTeamRound(0, 2, 1, 2);
-		assertEquals(false,game.isFirstPlayer());
-		
-		game.firstTeamRound(9, 2, 8, 2);
-		assertEquals(true,game.isFirstPlayer());
-		
-		game.letsPlay(1, 2, 2, 2);
-		assertEquals(false,game.isFirstPlayer());
-		
-		assertEquals(false,game.isEnded());
-		
-		for (int i = 0; i < 6; i++)
-			game.getFirstTeam()[i].setAlive(false);
-		assertEquals(false,game.firstWon());
-		
 		game.setFirstPlayer(false);
-		game.setBoard(game.getBoard());
-		game.setFirstTeam(game.getSecondTeam());
-		game.setSecondTeam(game.getFirstTeam());
 		assertEquals(false,game.isFirstPlayer());
-		assertEquals(game.getFirstTeam()[0],game.getSecondTeam()[0]);
-		assertEquals(board2,game.getBoard());
-		
-		Game game2 = new Game();
-		game2.initGame(secondTeam, firstTeam);
-		game2.letsPlay(0, 2, 1, 2);
-		game2.letsPlay(9, 2, 8, 2);
-		assertEquals(1,game2.getFirstTeam()[0].getUnitZone().getX());
-		assertEquals(8,game2.getSecondTeam()[0].getUnitZone().getX());
-
+		game.initGame(units2, units1);
+		game.initGame(units1, units2);
+		assertEquals(6,game.getFirstAlive());
+		game.setFirstTeam(game.getFirstTeam());
+		assertEquals(true,game.getFirstTeam()[0].isTeam());
+		game.setSecondTeam(game.getSecondTeam());
+		assertEquals(false,game.getSecondTeam()[0].isTeam());
+		game.setBoard(game.getBoard());
+		assertEquals(new Board().getZone(0, 0).getX(),game.getBoard().getZone(0, 0).getX());
+		game.firstTeamRound(0, 2, 1, 2);
+		assertEquals(new GameZone(1,2).getX(),game.getFirstTeam()[0].getUnitZone().getX());
+		game.secondTeamRound(9, 2, 8, 2);
+		assertEquals(new GameZone(8,2).getX(),game.getSecondTeam()[0].getUnitZone().getX());
+		game.letsPlay(1, 2, 2, 2);
+		assertEquals(new GameZone(2,2).getX(),game.getFirstTeam()[0].getUnitZone().getX());
+		assertEquals(false,game.isEnded());
+		assertEquals(true,game.firstWon());
+		game.moveAI();
+		assertEquals(new GameZone(7,2).getX(),game.getSecondTeam()[0].getUnitZone().getX());
 	}
 	
 	@Test
-	public void testGameUtility(){
-		CombatUnit unit1 = new Peasant(true,board.getZone(5, 5));
-		CombatUnit unit2 = new Peasant(true,board.getZone(5, 4));
-		CombatUnit unit3 = new Peasant(false,board.getZone(5, 5));
-		CombatUnit unit4 = new Peasant(true,board.getZone(5, 5));
-		CombatUnit unit5 = new Peasant(false,board.getZone(4, 4));
-		CombatUnit unit11 = new Cavalry(false,board.getZone(0, 0));
-		CombatUnit unit12 = new Guard(false,board.getZone(1, 0));
-		CombatUnit unit13 = new Infantry(false,board.getZone(2, 0));
-		CombatUnit unit14 = new Lancers(false,board.getZone(3, 0));
-		CombatUnit unit15 = new Militia(false,board.getZone(4, 0));
-		CombatUnit unit16 = new Paladin(false,board.getZone(5, 0));
-		CombatUnit unit17 = new Raiders(false,board.getZone(6, 0));
-		CombatUnit unit18 = new Spearman(false,board.getZone(7, 0));
-		CombatUnit unit19 = new Swordsmen(false,board.getZone(8, 0));
-
-		assertEquals(false,GameUtility.sameUnit(unit11, unit12));
-		assertEquals(false,GameUtility.sameUnit(unit13, unit14));
-		assertEquals(false,GameUtility.sameUnit(unit15, unit16));
-		assertEquals(false,GameUtility.sameUnit(unit17, unit18));
-		assertEquals(true,GameUtility.sameUnit(unit19, unit19));
-		
-		game.initGame(firstTeam, firstTeam);
-		
-		assertEquals(3,GameUtility.distanceBetween(board.getZone(0, 0),board.getZone(2, 3)));
-		assertEquals(true,GameUtility.isValidPos(1, 1));
-		assertEquals(false,GameUtility.isValidPos(10, 1));
-		assertEquals(true,GameUtility.isValidMove(board.getZone(1, 1), board.getZone(2, 1)));
-		assertEquals(false,GameUtility.isValidMove(board.getZone(1, 1), board.getZone(3, 1)));
-		assertEquals(true,GameUtility.sameZone(board.getZone(1, 1), board.getZone(1, 1)));
-		assertEquals(false,GameUtility.sameZone(board.getZone(1, 1), board.getZone(2, 1)));
-		assertEquals(true,GameUtility.sameZone(unit1.getUnitZone(), unit4.getUnitZone()));
-		assertEquals(false,GameUtility.sameUnit(unit1, unit2));
-		assertEquals(false,GameUtility.sameUnit(unit1, unit3));
-		assertEquals(true,GameUtility.sameUnit(unit1, unit4));
-		assertEquals(true,GameUtility.sameUnit(unit1, GameUtility.fightWinner(unit1, unit2)));
-		assertEquals(true,GameUtility.isValidAttack(unit1, unit5));
-		assertEquals(false,GameUtility.isValidAttack(unit1, unit2));
-		assertEquals(true,GameUtility.sameUnit(game.getFirstTeam()[0],
-				GameUtility.getUnitByZone(board.getZone(0, 2), game.getFirstTeam(), game.getSecondTeam())));
-		
-		GameUtility.moveUnitAdv(game.getFirstTeam()[0], game.getBoard().getZone(1, 2), 
+	public void gameUtilityTest(){
+		assertEquals(1,GameUtility.distanceBetween(new GameZone(0,0), new GameZone(1,1)));
+		assertEquals(true,GameUtility.isValidPos(0, 0));
+		assertEquals(true,GameUtility.isValidMove(new GameZone(0,0,true), new GameZone(1,1,true)));
+		assertEquals(true,GameUtility.sameZone(new GameZone(1,1), new GameZone(1,1)));
+		assertEquals(true,GameUtility.sameUnit(new CombatUnit(true,new GameZone(1,1)),
+				new CombatUnit(true,new GameZone(1,1))));
+		assertEquals(true,GameUtility.sameUnit(new Cavalry(true,new GameZone(1,1)),
+				GameUtility.fightWinner(new Cavalry(true,new GameZone(1,1)),
+						new Peasant(false,new GameZone(2,1)))));
+		assertEquals(true,GameUtility.isValidAttack(new CombatUnit(true,new GameZone(1,2)),
+				new CombatUnit(false,new GameZone(2,2))));
+		List<String> units1 = Arrays.asList("Peasant","Cavalry","Guard","Infantry","Lancers","Militia");
+		List<String> units2 = Arrays.asList("Paladin","Raiders","Spearman","Swordsmen","Lancers","Militia");
+		Game game = new Game();
+		game.initGame(units1, units2);
+		assertEquals(true,GameUtility.sameUnit(game.getFirstTeam()[0], 
+				GameUtility.getUnitByZone(game.getFirstTeam()[0].getUnitZone(),
+				game.getFirstTeam(),game.getSecondTeam())));
+		GameUtility.moveUnitAdv(game.getFirstTeam()[0], new GameZone(1,2,true),
 				game.getFirstTeam(), game.getSecondTeam());
-		assertEquals(true,GameUtility.sameZone(game.getFirstTeam()[0].getUnitZone(),
-				game.getBoard().getZone(1, 2)));
+		assertEquals(true,GameUtility.sameZone(new GameZone(1,2,false),game.getFirstTeam()[0].getUnitZone()));
 	}
-
 }
